94907403f7e217f672a37d393ec400bd010c309c
Mani Kanta <manikanta@Manis-MacBook-Air.local>
2025-04-27 22:02:16 +0530

    project completed

---
+++ frontend/postcss.config.js
@@ -0,0 +1,15 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+/*export default {
+    plugins: [
+      require('@tailwindcss/postcss'),
+      require('autoprefixer'),
+    ],
+  };*/
+  export default {
+    plugins: {
+      "@tailwindcss/postcss": {},
+    },
+  };
+  
+  
