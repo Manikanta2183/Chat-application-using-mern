8c97f443cb507073315a60a37a6e47e9087b1648
Mani Kanta <manikanta@Manis-MacBook-Air.local>
2025-04-27 23:20:16 +0530

    1

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -116,20 +116,3 @@ export const checkAuth = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
-/*export const signup = (req,res)=> {
-    const {fullName,email,password} = req.body;
-    try {
-        
-    } catch (error) {
-        
-    }
-};
-
-export const login = (req,res)=> {
-    res.send("login route");
-};
-
-export const logout = (req,res)=> {
-    res.send("logout route");
-};*/
