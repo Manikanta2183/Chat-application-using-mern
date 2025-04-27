180c766435224869cf107abb29f37dad23891ca5
Mani Kanta <manikanta@Manis-MacBook-Air.local>
2025-04-28 00:23:15 +0530

    Fix root route

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,7 +46,7 @@ app.use("/api/messages", messageRoutes);
   });
 }*/
 
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "frontend", "dist");
 
   // Serve static files
@@ -60,6 +60,14 @@ if (process.env.NODE_ENV === "production") {
   // Catch-all route for all other routes
   app.get("/*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}*/
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*path", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
